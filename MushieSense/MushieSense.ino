6fa1ffbf244841d804e5e16c6f93d4fa9937cab7
sector23-bob <76463069+sector23-bob@users.noreply.github.com>
2020-12-26 20:16:50 -0600

    Update MushieSense.ino
    Broke out functions, added CO2 placeholders

--- MushieSense/MushieSense.ino
+++ MushieSense/MushieSense.ino
@@ -16,7 +16,7 @@
 #define WHITE 0x7
 
 // Not Colors
-#define THRESHOLD 30 // Keep this around 30 to keep the SHT31 happy
+#define THRESHOLD 30 // Loop threshold for housekeeping. Keep this around 30 to keep the SHT31 happy.
 #define MAXTEMP 90.0
 #define MINTEMP 65.0
 #define MAXHUM 90.0
@@ -24,34 +24,101 @@
 #define MAXCO2 0.0 // TODO - set this ref. Stamets
 #define MINCO2 0.0 // TODO - set this ref. Stamets
 
+// LCD display
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+// Temp/humidity sensor
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
-RTC_PCF8523 rtc;
-
+// Enable/disable heater on SHT31
 bool enableHeater = false;
 
-uint8_t loopCnt = 0;
-uint8_t i = 0;
+// Real-time clock
+RTC_PCF8523 rtc;
 
+uint8_t loopCnt = 0;
+
+// Arrays for sensor readings
 float tmpVals[THRESHOLD];
 float humVals[THRESHOLD];
+float co2Vals[THRESHOLD];
 
-void lcdDisplay(float tmpAvg, float humAvg) {
+/*!
+  @brief Read LCD buttons - slug code 
+ */
+void handleButtons() {
+  uint8_t buttons = lcd.readButtons();
+  
+  if (buttons) {
+    if (buttons & BUTTON_UP) {
+      //lcd.setBacklight(RED);
+    }
+    if (buttons & BUTTON_DOWN) {
+      //lcd.setBacklight(YELLOW);
+    }
+    if (buttons & BUTTON_LEFT) {
+      //lcd.setBacklight(GREEN);
+    }
+    if (buttons & BUTTON_RIGHT) {
+      //lcd.setBacklight(VIOLET);
+    }
+    if (buttons & BUTTON_SELECT) {
+      //lcd.setBacklight(BLUE);
+    }
+  }
+}
+
+/*!
+  @brief Updates serial and LCD displays, if present TODO
+  @param tmpAvg Average temperature calculated
+  @param humAvg Average humidity calculated
+  @param co2Avg Average CO2 calculated TODO
+ */
+void updateDisplay(float tmpAvg, float humAvg, float co2Avg) {
     lcd.setCursor(0,0);
     lcd.print("Temp: "); lcd.print(tmpAvg); lcd.print(" F");
     Serial.print("Temp: "); Serial.print(tmpAvg); Serial.print("\t\t");
+    
     lcd.setCursor(0,1);
     lcd.print("Hum: "); lcd.print(humAvg); lcd.print("%");
     Serial.print("Hum: "); Serial.println(humAvg);
+}
 
-    // Freak out if something's off
-    if (((tmpAvg > MAXTEMP) or (tmpAvg < MINTEMP)) or ((humAvg > MAXHUM) or (humAvg < MINHUM))) {
+/*!
+  @brief Do something if calculated values exceed defined parameters TODO
+  @param tmpAvg Average temperature calculated
+  @param humAvg Average humidity calculated
+  @param co2Avg Average CO2 calculated TODO
+ */
+void freakOut(float tmpAvg, float humAvg, float co2Avg) {
+    bool tmpExceed = ((tmpAvg > MAXTEMP) or (tmpAvg < MINTEMP));
+    bool humExceed = ((humAvg > MAXHUM) or (humAvg < MINHUM));
+    bool co2Exceed = ((co2Avg > MAXCO2) or (co2Avg < MINCO2));
+    if (tmpExceed or humExceed or co2Exceed) {
       lcd.setBacklight(RED);
     } else {
       lcd.setBacklight(GREEN);
     }
 }
+
+/*!
+  @brief Functionality to handle sensor errors TODO
+ */
+void handleSensorError() {
+  ;
+}
+
+/*!
+  @brief Log data to long-term storage TODO
+  @param tmpAvg Average temperature calculated
+  @param humAvg Average humidity calculated
+  @param co2Avg Average CO2 calculated TODO
+  @param not Time of logging event
+ */
+void logData(float tmpAvg, float humAvg, float co2Avg, DateTime now) {
+  ;
+}
+
 void setup() {
   Serial.begin(9600);
   while (!Serial) {
@@ -61,9 +128,10 @@ void setup() {
   Serial.println("Initializing...");
   int time = millis();
 
-  /*** Initialize variables ***/
+  /*** Initialize sensor arrays ***/
   memset(tmpVals, 0.0, sizeof(tmpVals));
   memset(humVals, 0.0, sizeof(humVals));
+  memset(co2Vals, 0.0, sizeof(co2Vals));
 
   /*** Initialize LCD ***/
   lcd.begin(16, 2);
@@ -99,6 +167,8 @@ void setup() {
     Serial.println("ENABLED");
   else
     Serial.println("DISABLED");
+  // End SHT31
+    
   /*** End sensors test ***/
   
   time = millis() - time; // TODO - log this
@@ -109,34 +179,13 @@ void setup() {
 
 
 void loop() {
-  DateTime now = rtc.now();
-  
-  /*** Read LCD buttons - slug code for later ***/
-  uint8_t buttons = lcd.readButtons();
-  
-  if (buttons) {
-    if (buttons & BUTTON_UP) {
-      //lcd.setBacklight(RED);
-    }
-    if (buttons & BUTTON_DOWN) {
-      //lcd.setBacklight(YELLOW);
-    }
-    if (buttons & BUTTON_LEFT) {
-      //lcd.setBacklight(GREEN);
-    }
-    if (buttons & BUTTON_RIGHT) {
-      //lcd.setBacklight(VIOLET);
-    }
-    if (buttons & BUTTON_SELECT) {
-      //lcd.setBacklight(BLUE);
-    }
-  }
-  /*** End LCD button handling ***/
+  handleButtons();
   
   /*** Read sensor vals ***/
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
-
+  float c = 0.0; // CO2 sensor TODO
+  
   if (! isnan(t)) {
     tmpVals[loopCnt % THRESHOLD] = t;
   } else { 
@@ -148,31 +197,35 @@ void loop() {
   } else { 
     Serial.println("Failed to read humidity");
   }
+
+  if (! isnan(c)) {
+    co2Vals[loopCnt % THRESHOLD] = h;
+  } else {
+    Serial.println("Failed to read CO2");
+  }
   /*** End sensor read ***/
   
-  delay(100);
-
-  
-  /*** Do some housekeeping - maintenance, logging, and output - once the threshold is reached ***/
+  /*** Do some housekeeping - maintenance, logging, and output - once the loop count threshold is reached ***/
   if (++loopCnt == THRESHOLD) {
     lcd.clear();
 
     // Get averages
     float tmpSum = 0.0;
     float humSum = 0.0;
+    float co2Sum = 0.0;
 
-    for (i = 0; i < THRESHOLD; i++) {
+    for (int i = 0; i < THRESHOLD; i++) {
       tmpSum += tmpVals[i];
+      humSum += humVals[i];
+      co2Sum += co2Vals[i];
     }
     float tmpAvg = (tmpSum/THRESHOLD)*1.8 + 32.0;
-
-    for (i = 0; i < THRESHOLD; i++) {
-      humSum += humVals[i];
-    }
     float humAvg = humSum/THRESHOLD;
+    float co2Avg = co2Sum/THRESHOLD;
 
     // Display/log averages
-    lcdDisplay(tmpAvg, humAvg);
+    updateDisplay(tmpAvg, humAvg, co2Avg);
+    logData(tmpAvg, humAvg, co2Avg, rtc.now());
 
     // Do that maintenance
     enableHeater = !enableHeater;
@@ -183,8 +236,13 @@ void loop() {
     else
       Serial.println("DISABLED");
 
-    // Reset the loop
-    loopCnt = 0;
+    // Reset the loop once our count gets high enough, no need to use more than a byte
+    if ((loopCnt % 240) == 0) {
+      loopCnt = 0;
+    }
   }
   /*** End housekeeping ***/
+    
+  delay(100); // Read at ~10Hz
+
 }
