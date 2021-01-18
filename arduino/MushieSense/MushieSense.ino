6e808a1c657e3ba57e3a77ac2f250f69b1ad1884
sector23-bob <76463069+sector23-bob@users.noreply.github.com>
2021-01-17 20:58:51 -0600

    Update MushieSense.ino
    Headless operation

--- arduino/MushieSense/MushieSense.ino
+++ arduino/MushieSense/MushieSense.ino
@@ -179,9 +179,6 @@ void doLogging() {
 
 void setup() {
   Serial.begin(115200);
-  while (! Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
   
   Serial.println("Initializing...");
   loopTime = millis();
